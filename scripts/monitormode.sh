98560486dd0895eed6b780979156cbe9d90bcdf5
Jonathan <Jonathan+Git@Jacksonengineering.net>
2023-01-11 15:17:41 -0600

    add example command for monitoring beacons

--- scripts/monitormode.sh
+++ scripts/monitormode.sh
@@ -78,3 +78,4 @@ fi
 #sudo tshark --interface wlan1 --monitor-mode -f "broadcast or multicast"
 #sudo airodump-ng --beacons --ht20 --channel 6  wlan1 
 #sudo airodump-ng --beacons --ht20 --channel 6 -w wlan12022-12-21.pcap --output-format pcap,logcsv  wlan1
+#sudo tcpdump -vv -i wlan1 -e -XX type mgt subtype beacon
